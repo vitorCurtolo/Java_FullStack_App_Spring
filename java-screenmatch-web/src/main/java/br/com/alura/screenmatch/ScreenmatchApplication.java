58c5045b39428ea3d0497194eced99097b4d0312
Vitor Curtolo <vitor.curtolo09@gmail.com>
2024-05-30 11:56:39 -0300

    fullstack app

---
+++ java-screenmatch-web/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ScreenmatchApplication.class, args);
+    }
+}
